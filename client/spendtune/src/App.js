aa7404fb6086ebcef329f44be5bf54df403bc1e8
Kamil <kzmuda888@gmail.com>
2023-11-09 12:15:56 +0000

    F: implemented login, added register&dashboard components

--- client/spendtune/src/App.js
+++ client/spendtune/src/App.js
@@ -1,13 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+import Login from './components/login';
+import Register from './components/register';
+import Dashboard  from './components/dashboard';
 
 function App() {
+
+  const [logged, setLogged] = useState(null);
+
   return (
     <div className="App">
-      <h1>Spend Tune</h1>
-
+      <Router>
+        <h1>Spend Tune</h1>
+        { logged === null &&
+          <div>
+              <Login setLogged={setLogged}/>
+              <Register setLogged={setLogged}/>
+          </div>
+        }
+        {
+          logged !== null &&
+          <Dashboard logged={logged} setLogged={setLogged}/>
+        }
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
---
+++ client/spendtune/src/apiService.jsx
@@ -0,0 +1,19 @@
+const baseUrl = 'http://localhost:3001';
+
+async function logUser({ email, password }) {
+  try {
+    const userData = await fetch(baseUrl + '/login', {
+      method: 'POST',
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({email, password})
+    })
+    const user = await userData.json();
+    return user;
+  } catch (error) {
+    console.log('We have a problem:' + error)
+  }
+}
+
+export { logUser }
