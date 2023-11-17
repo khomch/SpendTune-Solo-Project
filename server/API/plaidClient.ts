2393559430be17ccabc8ff1d7823da535aeba1f6
Juan Vasquez <juancamilovasquezj@gmail.com>
2023-11-17 11:54:21 +0000

    switch secrets

--- server/API/plaidClient.ts
+++ server/API/plaidClient.ts
@@ -5,8 +5,8 @@ const config = new Configuration({
   basePath: PlaidEnvironments[process.env.PLAID_ENV || 'sandbox'],
   baseOptions: {
     headers: {
-      "PLAID-CLIENT-ID": process.env.PLAID_CLIENT_ID || '6555cd77866daa001cb98e37',
-      "PLAID-SECRET": process.env.PLAID_SECRET || '7b2c414be017c00787849a9f378373',
+      "PLAID-CLIENT-ID": process.env.PLAID_CLIENT_ID,
+      "PLAID-SECRET": process.env.PLAID_SECRET,
       "Plaid-Version": "2020-09-14",
     },
   },
